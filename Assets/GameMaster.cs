dd54874e657384954dc415189dc77c8ff9f4cbdb
Fenotyp <martin.vanky@gmail.com>
2019-11-15 14:19:03 +0100

    First commit

---
+++ Assets/GameMaster.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameMaster : MonoBehaviour
+{
+    public GameObject mousePointer;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKey(KeyCode.Mouse0))
+        {
+            Vector3 screenPoint = Input.mousePosition;
+            screenPoint.z = 10.0f; //distance of the plane from the camera
+           // transform.position = Camera.main.ScreenToWorldPoint(screenPoint);
+
+            mousePointer.transform.position = Camera.main.ScreenToWorldPoint(screenPoint); ;
+        }
+    }
+}
---
+++ Assets/Player.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    //public GameObject target;
+
+    private HingeJoint joint;
+    private Vector3 startPos;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        joint = GetComponent<HingeJoint>();
+        startPos = gameObject.transform.position;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (joint != null)
+        {
+            Debug.DrawLine(transform.position, joint.anchor + startPos);
+        }
+
+        if (joint != null && Physics.Linecast(transform.position, joint.anchor + startPos))
+        {
+            Debug.Log("blocked");
+
+            Destroy(joint);
+            GetComponent<SphereCollider>().enabled = true;
+        }
+    }
+}
