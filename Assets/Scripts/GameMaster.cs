766040cb5ef088b71f280a08f7441d75e651dc07
Martin <martin.vanky@gmail.com>
2019-11-18 17:11:08 +0100

    Restructured the files and added tags checks in the Player script

---
+++ Assets/Scripts/GameMaster.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameMaster : MonoBehaviour
+{
+    public GameObject mousePointer;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        // Make the mouse marker follow the position of the mouse
+        if (Input.GetKey(KeyCode.Mouse0))
+        {
+            Vector3 screenPoint = Input.mousePosition;
+            screenPoint.z = 10.0f; //distance of the plane from the camera
+
+            mousePointer.transform.position = Camera.main.ScreenToWorldPoint(screenPoint);
+        }
+    }
+}
---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,114 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    public GameObject Girl;
+    public float force_magn = 1.0f;
+    public float maxSpeed = 20f;//Replace with your max speed
+
+    private HingeJoint joint;
+    private Vector3 startPos;
+    private bool hasCollided;
+    private bool foundPartner;
+    private bool inGoal;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        joint = GetComponent<HingeJoint>();
+        startPos = gameObject.transform.position;
+
+       // girlForce = Girl.transform.position;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (joint != null)
+        {
+            Debug.DrawLine(transform.position, joint.anchor + startPos); // Olny for debugging
+        }
+
+        // Cut the join if the mouse is dragged over it
+        if (joint != null && Physics.Linecast(transform.position, joint.anchor + startPos))
+        {
+            Debug.Log("blocked");
+
+            Destroy(joint);
+            GetComponent<SphereCollider>().enabled = true;
+        }
+
+
+        if (!hasCollided && !inGoal)
+        {
+            // Swing Players
+            if (Input.GetKey("left"))
+            {
+
+                if (Girl.GetComponent<Rigidbody>().velocity.magnitude < maxSpeed)
+                {
+                    Girl.GetComponent<Rigidbody>().AddForce(-force_magn, 0, 0);
+                    //Debug.Log("Speed" + Girl.GetComponent<Rigidbody>().velocity.magnitude);
+                }
+            }
+
+            if (Input.GetKey("right"))
+            {
+
+                if (Girl.GetComponent<Rigidbody>().velocity.magnitude < maxSpeed)
+                {
+                    Girl.GetComponent<Rigidbody>().AddForce(force_magn, 0, 0); //gånger cos(vinkel)
+                    //Debug.Log("Speed" + Girl.GetComponent<Rigidbody>().velocity.magnitude);
+                }
+            }
+        }
+
+
+        Debug.Log("Object: " + hasCollided);
+        Debug.Log("Goal: " + inGoal);
+        Debug.Log("Player: " + foundPartner);
+    }
+
+
+    private void OnCollisionEnter(Collision other)
+    {
+        //Debug.Log("Collision " + other.gameObject.tag);
+
+        if (other.gameObject.tag == "Object")
+        {
+            hasCollided = true;
+        }
+
+        if (other.gameObject.tag == "Goal")
+        {
+            inGoal = true;
+        }
+
+        if (other.gameObject.tag == "Player")
+        {
+            foundPartner = true;
+            // Add spring between players
+        }
+    }
+
+    private void OnCollisionExit(Collision other)
+    {
+        if (other.gameObject.tag == "Object")
+        {
+            hasCollided = false;
+        }
+
+        if (other.gameObject.tag == "Goal")
+        {
+            inGoal = false;
+        }
+
+        if (other.gameObject.tag == "Player")
+        {
+            foundPartner = false;
+        }
+    }
+}
