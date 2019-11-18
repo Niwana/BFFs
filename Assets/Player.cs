f73355d194c2528791e8fbdd5d36e79d0df46eda
Angela Pena <Angie@MacBookAir-97.local>
2019-11-18 12:30:11 +0100

    Basic scene, can swing right and left with the arrow but hinge joint breaks if it goes to fast

--- Assets/Player.cs
+++ Assets/Player.cs
@@ -4,16 +4,21 @@ using UnityEngine;
 
 public class Player : MonoBehaviour
 {
+    public GameObject Girl;
+    public float force_magn = 1.0f;
     //public GameObject target;
 
     private HingeJoint joint;
     private Vector3 startPos;
+    public float maxSpeed = 20f;//Replace with your max speed
 
     // Start is called before the first frame update
     void Start()
     {
         joint = GetComponent<HingeJoint>();
         startPos = gameObject.transform.position;
+
+       // girlForce = Girl.transform.position;
     }
 
     // Update is called once per frame
@@ -30,6 +35,28 @@ public class Player : MonoBehaviour
 
             Destroy(joint);
             GetComponent<SphereCollider>().enabled = true;
+        }
+
+
+
+        if (Input.GetKey("left"))
+        {
+
+            if (Girl.GetComponent<Rigidbody>().velocity.magnitude < maxSpeed)
+            {
+                Girl.GetComponent<Rigidbody>().AddForce(-force_magn, 0, 0);
+                Debug.Log("Speed" + Girl.GetComponent<Rigidbody>().velocity.magnitude);
+            }
+        }
+
+        if (Input.GetKey("right"))
+        {
+
+            if (Girl.GetComponent<Rigidbody>().velocity.magnitude < maxSpeed)
+            {
+                Girl.GetComponent<Rigidbody>().AddForce(force_magn, 0, 0); //gånger cos(vinkel)
+                Debug.Log("Speed" + Girl.GetComponent<Rigidbody>().velocity.magnitude);
+            }
         }
     }
 }
