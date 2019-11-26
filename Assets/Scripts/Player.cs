2f22b7819ff6d1575e093d92af38dd4ea1dd2a7f
Angela Pena <Angie@MacBookAir-97.local>
2019-11-26 12:17:18 +0100

    added fixedUpdate

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -34,7 +34,7 @@ public class Player : MonoBehaviour
     }
 
     // Update is called once per frame
-    void Update()
+    void FixedUpdate()
     {
         // Cut the join if the mouse is dragged over it
         
